0c739a73fbc528bbd4d4636556bfe207f7bc6c36
Chinedu Arinze <c.s.arinze@gmail.com>
2019-09-04 01:55:49 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,7 +13,6 @@ class PetsController < ApplicationController
   post '/pets' do
     # binding.pry
     @pet = Pet.create(params[:pet])
-    binding.pry
     if !params["owner"]["name"].empty?
       @owner = Owner.create(params["owner"])
       @owner.save
