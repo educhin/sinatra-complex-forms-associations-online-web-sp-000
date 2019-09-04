d5bcbe026459b6e1ee38dacecf7b66217951817d
Chinedu Arinze <c.s.arinze@gmail.com>
2019-09-04 02:37:36 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -38,7 +38,7 @@ class PetsController < ApplicationController
   patch '/pets/:id' do
 
     @pet = Pet.find(params[:id])
-    @pet.update(params[pet])
+    @pet.update(params[:pet])
 
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(params["owner"])
